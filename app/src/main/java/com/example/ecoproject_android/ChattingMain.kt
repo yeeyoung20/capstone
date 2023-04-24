99389dd189b5f3203ad08dd6f26358b8e55aa5d4
yeeyoung20 <tlsdldud00@gmail.com>
2023-04-24 17:49:41 +0900

    2

--- app/src/main/java/com/example/ecoproject_android/ChattingMain.kt
+++ app/src/main/java/com/example/ecoproject_android/ChattingMain.kt
@@ -1,11 +1,21 @@
 package com.example.ecoproject_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class ChattingMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting_main)
+
+        val back=findViewById<ImageButton>(R.id.back)
+
+        //뒤로가기
+        back.setOnClickListener{
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
