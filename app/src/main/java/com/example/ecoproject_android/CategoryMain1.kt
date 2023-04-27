be2e8d638531fee0aaaf7f38a82bc5b66e6686b0
yeeyoung20 <tlsdldud00@gmail.com>
2023-04-27 14:54:02 +0900

    가구/인테리어 부연설명 전부 집어넣음

--- app/src/main/java/com/example/ecoproject_android/CategoryMain1.kt
+++ app/src/main/java/com/example/ecoproject_android/CategoryMain1.kt
@@ -14,15 +14,116 @@ class CategoryMain1 : AppCompatActivity() {
         setContentView(R.layout.activity_category_main1)
         val back=findViewById<ImageButton>(R.id.back)
         val wallpaper=findViewById<Button>(R.id.wallpaper)
+        val led=findViewById<Button>(R.id.led)
+        val furniture=findViewById<Button>(R.id.furniture)
+        val mirror=findViewById<Button>(R.id.mirror)
+        val tree=findViewById<Button>(R.id.tree)
+        val ceramic=findViewById<Button>(R.id.ceramic)
+        val latex=findViewById<Button>(R.id.latex)
+        val matrix=findViewById<Button>(R.id.matrix)
+        val cushion=findViewById<Button>(R.id.cushion)
+        val pillow=findViewById<Button>(R.id.pillow)
+        val sofa=findViewById<Button>(R.id.sofa)
+        val candle=findViewById<Button>(R.id.candle)
+        val chair=findViewById<Button>(R.id.chair)
+        val bedding=findViewById<Button>(R.id.bedding)
+        val bulb=findViewById<Button>(R.id.bulb)
+        val bed=findViewById<Button>(R.id.bed)
+        val cattower=findViewById<Button>(R.id.cattower)
+        val curtain=findViewById<Button>(R.id.curtain)
+        val paintbucket=findViewById<Button>(R.id.paintbucket)
+        val lamp=findViewById<Button>(R.id.lamp)
+        val pot=findViewById<Button>(R.id.pot)
+
 
         //뒤로가기
         back.setOnClickListener{
             val intent= Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        //벽지 누르면 분리배출 방법 안내 예시
+        //버튼 누르면 분리배출 방법 안내 예시
         wallpaper.setOnClickListener{
-            val intent= Intent(this, CategoryEx1::class.java)
+            val intent= Intent(this, Category1Wallpaper::class.java)
+            startActivity(intent)
+        }
+        led.setOnClickListener{
+            val intent= Intent(this, Category1Led::class.java)
+            startActivity(intent)
+        }
+        furniture.setOnClickListener{
+            val intent= Intent(this, Category1Furniture::class.java)
+            startActivity(intent)
+        }
+        mirror.setOnClickListener{
+            val intent= Intent(this, Category1Mirror::class.java)
+            startActivity(intent)
+        }
+        tree.setOnClickListener{
+            val intent= Intent(this, Category1Tree::class.java)
+            startActivity(intent)
+        }
+        ceramic.setOnClickListener{
+            val intent= Intent(this, Category1Ceramic::class.java)
+            startActivity(intent)
+        }
+        latex.setOnClickListener{
+            val intent= Intent(this, Category1Latex::class.java)
+            startActivity(intent)
+        }
+        matrix.setOnClickListener{
+            val intent= Intent(this, Category1Matrix::class.java)
+            startActivity(intent)
+        }
+        cushion.setOnClickListener{
+            val intent= Intent(this, Category1Cushion::class.java)
+            startActivity(intent)
+        }
+        pillow.setOnClickListener{
+            val intent= Intent(this, Category1Pillow::class.java)
+            startActivity(intent)
+        }
+        sofa.setOnClickListener{
+            val intent= Intent(this, Category1Sofa::class.java)
+            startActivity(intent)
+        }
+        candle.setOnClickListener{
+            val intent= Intent(this, Category1Candle::class.java)
+            startActivity(intent)
+        }
+        chair.setOnClickListener{
+            val intent= Intent(this, Category1Chair::class.java)
+            startActivity(intent)
+        }
+        bedding.setOnClickListener{
+            val intent= Intent(this, Category1Bedding::class.java)
+            startActivity(intent)
+        }
+        bulb.setOnClickListener{
+            val intent= Intent(this, Category1Bulb::class.java)
+            startActivity(intent)
+        }
+        bed.setOnClickListener{
+            val intent= Intent(this, Category1Bed::class.java)
+            startActivity(intent)
+        }
+        cattower.setOnClickListener{
+            val intent= Intent(this, Category1Cattower::class.java)
+            startActivity(intent)
+        }
+        curtain.setOnClickListener{
+            val intent= Intent(this, Category1Curtain::class.java)
+            startActivity(intent)
+        }
+        paintbucket.setOnClickListener{
+            val intent= Intent(this, Category1Paintbucket::class.java)
+            startActivity(intent)
+        }
+        lamp.setOnClickListener{
+            val intent= Intent(this, Category1Lamp::class.java)
+            startActivity(intent)
+        }
+        pot.setOnClickListener{
+            val intent= Intent(this, Category1Pot::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/ecoproject_android/MainActivity.kt
+++ app/src/main/java/com/example/ecoproject_android/MainActivity.kt
@@ -10,89 +10,89 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //테스트를 위해 추석처리함
-        //2빼고 주석 빼면됨
-        setContentView(R.layout.activity_main_2)
+        //2빼고 주석 빼면됨 ㅇㅋ
+        setContentView(R.layout.activity_main)
 
-//        val main1_1=findViewById<ImageButton>(R.id.main1_1)
-//        val main1_2=findViewById<ImageButton>(R.id.main1_2)
-//        val main1_3=findViewById<ImageButton>(R.id.main1_3)
-//        val main1_4=findViewById<ImageButton>(R.id.main1_4)
-//        val main1_5=findViewById<ImageButton>(R.id.main1_5)
-//        val main1_6=findViewById<ImageButton>(R.id.main1_6)
-//        val main1_7=findViewById<ImageButton>(R.id.main1_7)
-//        val main1_8=findViewById<ImageButton>(R.id.main1_8)
-//        val main1_9=findViewById<ImageButton>(R.id.main1_9)
-//        val main1_10=findViewById<ImageButton>(R.id.main1_10)
-//        val main1_11=findViewById<ImageButton>(R.id.main1_11)
-//        val main2_1=findViewById<ImageButton>(R.id.main2_1)
-//        val main2_2=findViewById<ImageButton>(R.id.main2_2)
-//        val main2_3=findViewById<ImageButton>(R.id.main2_3)
-//        val main2_4=findViewById<ImageButton>(R.id.main2_4)
-//
-//
-//
-//
-//        //메인화면에서 카테고리 이미지버튼 클릭했을 때 화면전환
-//        main1_1.setOnClickListener{
-//            val intent= Intent(this, CategoryMain1::class.java)
-//            startActivity(intent)
-//        }
-//        main1_2.setOnClickListener{
-//            val intent= Intent(this, CategoryMain2::class.java)
-//            startActivity(intent)
-//        }
-//        main1_3.setOnClickListener{
-//            val intent= Intent(this, CategoryMain3::class.java)
-//            startActivity(intent)
-//        }
-//        main1_4.setOnClickListener{
-//            val intent= Intent(this, CategoryMain4::class.java)
-//            startActivity(intent)
-//        }
-//        main1_5.setOnClickListener{
-//            val intent= Intent(this, CategoryMain5::class.java)
-//            startActivity(intent)
-//        }
-//        main1_6.setOnClickListener{
-//            val intent= Intent(this, CategoryMain6::class.java)
-//            startActivity(intent)
-//        }
-//        main1_7.setOnClickListener{
-//            val intent= Intent(this, CategoryMain7::class.java)
-//            startActivity(intent)
-//        }
-//        main1_8.setOnClickListener{
-//            val intent= Intent(this, CategoryMain8::class.java)
-//            startActivity(intent)
-//        }
-//        main1_9.setOnClickListener{
-//            val intent= Intent(this, CategoryMain9::class.java)
-//            startActivity(intent)
-//        }
-//        main1_10.setOnClickListener{
-//            val intent= Intent(this, CategoryMain10::class.java)
-//            startActivity(intent)
-//        }
-//        main1_11.setOnClickListener{
-//            val intent= Intent(this, CategoryMain11::class.java)
-//            startActivity(intent)
-//        }
-//        main2_1.setOnClickListener {
-//            val intent = Intent(this, CommunityMain::class.java)
-//            startActivity(intent)
-//        }
-//        main2_2.setOnClickListener {
-//            val intent = Intent(this, ChattingMain::class.java)
-//            startActivity(intent)
-//        }
-//        main2_3.setOnClickListener {
-//            val intent = Intent(this, Map::class.java)
-//            startActivity(intent)
-//        }
-//        main2_4.setOnClickListener{
-//            val intent= Intent(this, MyInfoChange::class.java)
-//            startActivity(intent)
-//        }
+        val main1_1=findViewById<ImageButton>(R.id.main1_1)
+        val main1_2=findViewById<ImageButton>(R.id.main1_2)
+        val main1_3=findViewById<ImageButton>(R.id.main1_3)
+        val main1_4=findViewById<ImageButton>(R.id.main1_4)
+        val main1_5=findViewById<ImageButton>(R.id.main1_5)
+        val main1_6=findViewById<ImageButton>(R.id.main1_6)
+        val main1_7=findViewById<ImageButton>(R.id.main1_7)
+        val main1_8=findViewById<ImageButton>(R.id.main1_8)
+        val main1_9=findViewById<ImageButton>(R.id.main1_9)
+        val main1_10=findViewById<ImageButton>(R.id.main1_10)
+        val main1_11=findViewById<ImageButton>(R.id.main1_11)
+        val main2_1=findViewById<ImageButton>(R.id.main2_1)
+        val main2_2=findViewById<ImageButton>(R.id.main2_2)
+        val main2_3=findViewById<ImageButton>(R.id.main2_3)
+        val main2_4=findViewById<ImageButton>(R.id.main2_4)
+
+
+
+
+        //메인화면에서 카테고리 이미지버튼 클릭했을 때 화면전환
+        main1_1.setOnClickListener{
+            val intent= Intent(this, CategoryMain1::class.java)
+            startActivity(intent)
+        }
+        main1_2.setOnClickListener{
+            val intent= Intent(this, CategoryMain2::class.java)
+            startActivity(intent)
+        }
+        main1_3.setOnClickListener{
+            val intent= Intent(this, CategoryMain3::class.java)
+            startActivity(intent)
+        }
+        main1_4.setOnClickListener{
+            val intent= Intent(this, CategoryMain4::class.java)
+            startActivity(intent)
+        }
+        main1_5.setOnClickListener{
+            val intent= Intent(this, CategoryMain5::class.java)
+            startActivity(intent)
+        }
+        main1_6.setOnClickListener{
+            val intent= Intent(this, CategoryMain6::class.java)
+            startActivity(intent)
+        }
+        main1_7.setOnClickListener{
+            val intent= Intent(this, CategoryMain7::class.java)
+            startActivity(intent)
+        }
+        main1_8.setOnClickListener{
+            val intent= Intent(this, CategoryMain8::class.java)
+            startActivity(intent)
+        }
+        main1_9.setOnClickListener{
+            val intent= Intent(this, CategoryMain9::class.java)
+            startActivity(intent)
+        }
+        main1_10.setOnClickListener{
+            val intent= Intent(this, CategoryMain10::class.java)
+            startActivity(intent)
+        }
+        main1_11.setOnClickListener{
+            val intent= Intent(this, CategoryMain11::class.java)
+            startActivity(intent)
+        }
+        main2_1.setOnClickListener {
+            val intent = Intent(this, CommunityMain::class.java)
+            startActivity(intent)
+        }
+        main2_2.setOnClickListener {
+            val intent = Intent(this, ChattingMain::class.java)
+            startActivity(intent)
+        }
+        main2_3.setOnClickListener {
+            val intent = Intent(this, Map::class.java)
+            startActivity(intent)
+        }
+        main2_4.setOnClickListener{
+            val intent= Intent(this, MyInfoChange::class.java)
+            startActivity(intent)
+        }
 
     }
 }
