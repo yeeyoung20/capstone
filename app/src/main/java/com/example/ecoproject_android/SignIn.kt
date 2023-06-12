c9d2f63e2968bb9fd572da36f64557ccef5c51a2
Ryudaeunn <daeun0501@naver.com>
2023-06-12 13:12:23 +0900

    '0612'

--- app/src/main/java/com/example/ecoproject_android/SignIn.kt
+++ app/src/main/java/com/example/ecoproject_android/SignIn.kt
@@ -8,7 +8,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.ecoproject_android.databinding.ActivitySignInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.user.UserApiClient
@@ -17,7 +27,9 @@ import com.navercorp.nid.oauth.NidOAuthLogin
 import com.navercorp.nid.oauth.OAuthLoginCallback
 import com.navercorp.nid.profile.NidProfileCallback
 import com.navercorp.nid.profile.data.NidProfileResponse
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class SignIn : AppCompatActivity() {
@@ -27,10 +39,14 @@ class SignIn : AppCompatActivity() {
     private var nick: String = ""
     private var name: String = ""
 
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private var tokenId: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
 
         //val keyHash = Utility.getKeyHash(this)
         //Log.d("Hash", keyHash)
@@ -63,7 +79,7 @@ class SignIn : AppCompatActivity() {
             auth.run { }
         }
 
-        //네이버 로그인 부분
+        //네이버 로그인
         naverbtn.setOnClickListener {
             val oAuthLoginCallback = object : OAuthLoginCallback {
                 override fun onSuccess() {
@@ -77,27 +93,22 @@ class SignIn : AppCompatActivity() {
                             Log.e(TAG, "네이버 로그인한 유저 정보 - 이메일 : $email")
                             Log.e(TAG, "네이버 로그인한 유저 정보 - 닉네임 : $nick")
                         }
-
                         override fun onError(errorCode: Int, message: String) {
                             //
                         }
-
                         override fun onFailure(httpStatus: Int, message: String) {
                             //
                         }
                     })
                 }
-
                 override fun onError(errorCode: Int, message: String) {
                     val naverAccessToken = NaverIdLoginSDK.getAccessToken()
                     Log.e(TAG, "naverAccessToken : $naverAccessToken")
                 }
-
                 override fun onFailure(httpStatus: Int, message: String) {
                     //
                 }
             }
-
             NaverIdLoginSDK.initialize(
                 this@SignIn,
                 getString(R.string.naver_client_id),
@@ -107,11 +118,12 @@ class SignIn : AppCompatActivity() {
             NaverIdLoginSDK.authenticate(this@SignIn, oAuthLoginCallback)
         }//네이버 로그인
 
-
         signup.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+
         //카카오 로그인
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
@@ -123,7 +135,8 @@ class SignIn : AppCompatActivity() {
                         Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
                     }
                     error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
-                        Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT)
+                            .show()
                     }
                     error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
                         Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
@@ -132,7 +145,8 @@ class SignIn : AppCompatActivity() {
                         Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
                     }
                     error.toString() == AuthErrorCause.Misconfigured.toString() -> {
-                        Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT)
+                            .show()
                     }
                     error.toString() == AuthErrorCause.ServerError.toString() -> {
                         Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
@@ -141,23 +155,67 @@ class SignIn : AppCompatActivity() {
                         Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
                     }
                 }
-            }
-            else if (token != null) {
+            } else if (token != null) {
                 Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, SignIn::class.java)
                 startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
                 finish()
             }
         }
-
         kakaologin.setOnClickListener {
-            if(UserApiClient.instance.isKakaoTalkLoginAvailable(this)){
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
                 UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
-            }else{
+            } else {
                 UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
             }
-        }
+        }//카카오 로그인
 
 
+        //구글 로그인
+        firebaseAuth = FirebaseAuth.getInstance()
+        launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(), ActivityResultCallback { result ->
+                Log.e(TAG, "resultCode : ${result.resultCode}")
+                Log.e(TAG, "result : $result")
+                if (result.resultCode == RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    try {
+                        task.getResult(ApiException::class.java)?.let { account ->
+                            tokenId = account.idToken
+                            if (tokenId != null && tokenId != "") {
+                                val credential: AuthCredential =
+                                    GoogleAuthProvider.getCredential(account.idToken, null)
+                                firebaseAuth.signInWithCredential(credential)
+                                    .addOnCompleteListener {
+                                        if (firebaseAuth.currentUser != null) {
+                                            val user: FirebaseUser = firebaseAuth.currentUser!!
+                                            email = user.email.toString()
+                                            Log.e(TAG, "email : $email")
+                                            val googleSignInToken = account.idToken ?: ""
+                                            if (googleSignInToken != "") {
+                                                Log.e(TAG, "googleSignInToken : $googleSignInToken")
+                                            } else {
+                                                Log.e(TAG, "googleSignInToken이 null")
+                                            }
+                                        }
+                                    }
+                            }
+                        } ?: throw Exception()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            })
+        googlelogin.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+                val googleSignInClient = GoogleSignIn.getClient(this@SignIn, gso)
+                val signInIntent: Intent = googleSignInClient.signInIntent
+                launcher.launch(signInIntent)
+            }
+        }//구글 로그인
     }
 }
